28b4a31100acb9326e94fc4459170a448f329ff6
Brian Ho <briankyho@gmail.com>
2017-10-19 20:10:42 -0700

    Refactor and created attachments and hyperlink

--- coursebot.py
+++ coursebot.py
@@ -1,4 +1,5 @@
 import os
+import json
 import time
 from slackclient import SlackClient
 from uwaterlooapi import UWaterlooAPI
@@ -42,17 +43,10 @@ def get_formatted_json(s):
     subject = s['subject']
     catalog_num = s['catalog_number']
     title = s['title']
-    prereqs = s['prerequisites']
-    antireqs = s['antirequisites']
-    desc = s['description']
     url = s['url']
 
-    return (("*" + subject + catalog_num + " - " + title + "*\n\n") \
-           if subject and catalog_num and title else "") + \
-           (("Prereqs: " + prereqs + "\n\n") if prereqs else "") + \
-           (("Antireqs: " + antireqs + "\n\n") if antireqs else "") + \
-           ((desc + "\n\n") if desc else "") + \
-           (url if url else "")
+    return (("<" + url + "|*" + subject + catalog_num + " - " + title + "*>") \
+           if url and subject and catalog_num and title else "")
 
 def handle_command(command, channel):
     response = "Invalid query. Note that a sample invocation would be " \
@@ -62,15 +56,38 @@ def handle_command(command, channel):
     course_json = course_info[0]
     valid_query = course_info[1]
 
+    attachment_resp = None
+
     if valid_query:
         if course_json:
             response = get_formatted_json(course_json)
+            attachment_resp = json.dumps([
+                {
+                    "fallback" : "Error retrieving course description",
+                    "text" : course_json["description"],
+                    "color" : "good"
+                },
+                ({
+                    "fields": [{
+                        "title": "Prereqs",
+                        "value": course_json["prerequisites"]
+                    }],
+                    "color" : "warning"
+                } if course_json["prerequisites"] else None),
+                ({
+                    "fields": [{
+                        "title": "Antireqs",
+                        "value": course_json["antirequisites"]
+                    }],
+                    "color" : "danger"
+                } if course_json["antirequisites"] else None)
+            ])
         else:
             response = "Sorry, I have no data on this course."
 
     SLACK_CLIENT.api_call("chat.postMessage", channel=channel,
-                          text=response, as_user=True)
-
+                          text=response, as_user=True,
+                          attachments=attachment_resp)
 
 def parse_slack_output(slack_rtm_output):
     output_list = slack_rtm_output
@@ -81,9 +98,8 @@ def parse_slack_output(slack_rtm_output):
                        output['channel']
     return None, None
 
-
 if __name__ == "__main__":
-    SLEEP_DELAY = 0.5
+    SLEEP_DELAY = 1
 
     if SLACK_CLIENT.rtm_connect():
         print "Coursebot connected and running!"
@@ -93,7 +109,7 @@ if __name__ == "__main__":
 
             if COMMAND and CHANNEL:
                 handle_command(COMMAND, CHANNEL)
-                
+
             time.sleep(SLEEP_DELAY)
     else:
         print "Connection failed. Check Slack token or bot ID!"
